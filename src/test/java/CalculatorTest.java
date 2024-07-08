46025aadddcbbfd1fd32fe89dba1fa73f1af3837
Yonatan Embiza Tadesse <enyonathan@gmail.com>
2024-07-08 17:29:19 -0500

    the first commit to the TDD based development project

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+    @Test
+    public void testAddTwoIntegers() {
+        Calculator calculator = new Calculator();
+        int result = calculator.addTwoIntegers(2, 3);
+        assertEquals(5, result);
+    }
+    @Test
+    public void testSubtractTwoIntegers() {
+        Calculator calculator = new Calculator();
+        int result = calculator.subtractTwoIntegers(2, 3);
+        assertEquals(-1, result);
+    }
+    @Test
+    public void testMultiplyTwoIntegers(){
+        Calculator calculator = new Calculator();
+        double result = calculator.multiplyTwoIntegers(2, 3);
+        assertEquals(6, result);
+    }
+    @Test
+    public void testDivideTwoIntegers(){
+        Calculator calculator = new Calculator();
+        double result = calculator.divideTwoIntegers(6, 3);
+        assertEquals(2, result);
+    }
+}
