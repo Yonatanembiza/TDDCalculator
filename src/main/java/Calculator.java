0a4cc9c51f110959c718b8c6945ca2ceda5a40bd
Yonatan Embiza Tadesse <enyonathan@gmail.com>
2024-07-08 18:28:59 -0500

    Initial commit

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,17 @@
+public class Calculator {
+    public int addTwoIntegers(int a, int b) {
+        return a + b;
+    }
+    public int subtractTwoIntegers(int a, int b) {
+        return a - b;
+    }
+    public double multiplyTwoIntegers(int a, int b){
+        return a * b;
+    }
+    public double divideTwoIntegers(int a, int b){
+        if(b == 0){
+            throw new IllegalArgumentException("Cannot divide by zero");
+        }
+        return (double) a / b;
+    }
+}
